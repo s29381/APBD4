47e151fc5fc8ca6be1feaa1f310d6cb82beae29f
s29381 <s29381@pjwstk.edu.pl>
2024-04-22 00:05:30 +0200

    Working Project

---
+++ WebApplication1/WebApplication1/Model/Visit.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Controller;
+public class Visit()
+{
+    public string visitDate { get; set; }
+    public Animal visitAnimal { get; set; }
+    public string visitDescription { get; set; }
+    public double visitPrice { get; set; }
+}
